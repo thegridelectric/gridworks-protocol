b273d8d69676317b8543bb711b1dbb057efee2c6
Andrew Schweitzer <schweitz72@gmail.com>
2022-11-10 11:10:35 -0500

    Added ResponseTimeoutEvent, PeerActiveEvent, and CommEvent.PeerName

--- src/gwproto/messages/misc.py
+++ src/gwproto/messages/misc.py
@@ -1,8 +1,27 @@
+import uuid
+from typing import Any
 from typing import Literal
 
 from pydantic import BaseModel
+from pydantic import Field
+
+from gwproto.message import Message
 
 
 class Ack(BaseModel):
     AckMessageID: str
     TypeName: Literal["gridworks.ack"] = "gridworks.ack"
+
+
+class Ping(BaseModel):
+    MessageId: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    TypeName: Literal["gridworks.ping"] = "gridworks.ping"
+
+
+class PingMessage(Message[Ping]):
+    def __init__(self, **data: Any):
+        if "AckRequired" not in data:
+            data["AckRequired"] = True
+        if "Payload" not in data:
+            data["Payload"] = Ping()
+        super().__init__(**data)

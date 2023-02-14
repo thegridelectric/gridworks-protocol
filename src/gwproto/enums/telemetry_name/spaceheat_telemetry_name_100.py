c845e2f4f2a797e41032137ab796a30fe069bcc1
Jessica <jessica.lynn.millar@gmail.com>
2023-02-13 19:23:13 -0500

    Update TelemetryName for new power readings

--- src/gwproto/enums/telemetry_name/spaceheat_telemetry_name_100.py
+++ src/gwproto/enums/telemetry_name/spaceheat_telemetry_name_100.py
@@ -8,25 +8,31 @@ class TelemetryName(enum.Enum):
     @classmethod
     def values(cls):
         return [elt.value for elt in cls]
-
+    UNKNOWN = "Unknown"
     POWER_W = "PowerW"
-    WATER_TEMP_F_TIMES1000 = "WaterTempFTimes1000"
-    GALLONS_PER_MINUTE_TIMES10 = "GallonsPerMinuteTimes10"
-    WATER_FLOW_GPM_TIMES100 = "WaterFlowGpmTimes100"
-    WATER_TEMP_C_TIMES1000 = "WaterTempCTimes1000"
     RELAY_STATE = "RelayState"
+    WATER_TEMP_C_TIMES1000 = "WaterTempCTimes1000"
+    WATER_TEMP_F_TIMES1000 = "WaterTempFTimes1000"
+    WATER_FLOW_GPM_TIMES100 = "WaterFlowGpmTimes100"
     CURRENT_RMS_MICRO_AMPS = "CurrentRmsMicroAmps"
-    #
+    GALLONS_PER_MINUTE_TIMES10 = "GallonsPerMinuteTimes10"
+    VOLTAGE_RMS_MILLI_AMPS = "VoltageRmsMilliAmps"
+    MILLI_WATT_HOURS = "MilliWattHours"
+    FREQUENCY_MICRO_HZ = "FrequencyMicroHz"
+    
 
 
 class SpaceheatTelemetryName100GtEnum(ABC):
     symbols: List[str] = [
+        "00000000",
         "af39eec9",
-        "793505aa",
-        "329a68c0",
-        "d70cce28",
-        "c89d0ba1",
         "5a71d4b3",
+        "c89d0ba1",
+        "793505aa",
+        "d70cce28",
         "ad19e79c",
-        #
+        "329a68c0",
+        "bb6fdd59",
+        "e0bb014b",
+        "337b8659",
     ]

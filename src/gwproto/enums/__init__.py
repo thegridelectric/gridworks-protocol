d1f7c2a9bb278a4349579a0f796ae9abaf7535e2
Jessica <jessica.lynn.millar@gmail.com>
2023-02-24 05:02:23 -0500

    squash

--- src/gwproto/enums/__init__.py
+++ src/gwproto/enums/__init__.py
@@ -1,8 +1,10 @@
 """ GwSchema Enums used in gwproto """
 from gwproto.enums.telemetry_name import TelemetryName
+from gwproto.enums.telemetry_name_map import TelemetryNameMap
 
 
 __all__ = [
     "TelemetryName",
+    "TelemetryNameMap",
 ]
 

f84bee1dda68886c662ec72858a48afa2eeec617
Andrew Schweitzer <schweitz72@gmail.com>
2023-11-20 18:01:54 -0500

    Fix Role test

--- tests/enums/role_test.py
+++ tests/enums/role_test.py
@@ -24,6 +24,7 @@ def test_role() -> None:
         "DedicatedThermalStore",
         "TankWaterTempSensor",
         "PipeTempSensor",
+        "ThermostatHeatingSetPoint",
     }
 
     assert Role.default() == Role.Unknown

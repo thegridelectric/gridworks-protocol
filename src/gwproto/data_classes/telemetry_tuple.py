28323122470727bb32bbdecda554b4647ae72b2f
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-09-25 11:11:24 -0400

    enums are strings

--- src/gwproto/data_classes/telemetry_tuple.py
+++ src/gwproto/data_classes/telemetry_tuple.py
@@ -10,4 +10,4 @@ class TelemetryTuple(NamedTuple):
     TelemetryName: TelemetryName
 
     def __repr__(self) -> str:
-        return f"TT({self.AboutNode.alias} {self.TelemetryName.value} read by {self.SensorNode.alias})"
+        return f"TT({self.AboutNode.alias} {self.TelemetryName} read by {self.SensorNode.alias})"

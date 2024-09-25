0175134cb44d5fa2b8ec0cf0f76fa4038a69af1b
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-09-25 09:16:30 -0400

    EgaugeRegisterConfig can be missing from ElectricMeterChannelConfig

--- src/gwproto/types/electric_meter_channel_config.py
+++ src/gwproto/types/electric_meter_channel_config.py
@@ -4,11 +4,11 @@ from typing import Literal, Optional
 
 from gwproto.types.channel_config import ChannelConfig
 from gwproto.types.egauge_register_config import (
-    EgaugeRegisterConfig,
+    EgaugeRegisterConfig as EgaugeConfig,
 )
 
 
 class ElectricMeterChannelConfig(ChannelConfig):
-    EgaugeRegisterConfig: Optional[EgaugeRegisterConfig]
+    EgaugeRegisterConfig: Optional[EgaugeConfig] = None
     TypeName: Literal["electric.meter.channel.config"] = "electric.meter.channel.config"
     Version: Literal["000"] = "000"

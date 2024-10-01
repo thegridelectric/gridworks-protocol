33bfa6bfb415a7a008251e5bf2a1d1d8ad6cf3e5
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-10-01 15:43:39 -0400

    Add ChannelStub

--- src/gwproto/data_classes/telemetry_tuple.py
+++ src/gwproto/data_classes/telemetry_tuple.py
@@ -1,7 +1,10 @@
 from typing import NamedTuple
 
+from pydantic import BaseModel
+
 from gwproto.data_classes.sh_node import ShNode
 from gwproto.enums import TelemetryName
+from gwproto.property_format import SpaceheatName
 
 
 class TelemetryTuple(NamedTuple):
@@ -11,3 +14,13 @@ class TelemetryTuple(NamedTuple):
 
     def __repr__(self) -> str:
         return f"TT({self.AboutNode.alias} {self.TelemetryName} read by {self.SensorNode.alias})"
+
+
+class ChannelStub(BaseModel):
+    Name: SpaceheatName
+    AboutNodeName: SpaceheatName
+    CapturedByNodeName: SpaceheatName
+    TelemetryName: TelemetryName
+
+    def __hash__(self) -> int:
+        return hash(self.Name)

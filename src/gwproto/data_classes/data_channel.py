214cef8c2e4ea48ae64d496db62a210326560a0a
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-10-11 11:24:06 -0400

    try again with a to_gt method for DataChannel

--- src/gwproto/data_classes/data_channel.py
+++ src/gwproto/data_classes/data_channel.py
@@ -12,3 +12,9 @@ class DataChannel(DataChannelGt):
 
     def __hash__(self) -> int:
         return hash(self.Id)
+
+    def to_gt(self) -> DataChannelGt:
+        # Copy the current instance excluding the extra fields
+        return DataChannelGt(
+            **self.model_dump(exclude={"about_node", "captured_by_node"})
+        )

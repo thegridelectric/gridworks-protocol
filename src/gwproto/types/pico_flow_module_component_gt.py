f743c0d41174cd907bd4ed5bf56d1a35e8ee904b
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-10-25 11:45:34 -0400

    pico flow module includes flow node name

--- src/gwproto/types/pico_flow_module_component_gt.py
+++ src/gwproto/types/pico_flow_module_component_gt.py
@@ -1,12 +1,14 @@
 from typing import Literal, Optional
 
 from gwproto.enums import GpmFromHzMethod, HzCalcMethod, MakeModel
+from gwproto.property_format import SpaceheatName
 from gwproto.types.component_gt import ComponentGt
 
 
 class PicoFlowModuleComponentGt(ComponentGt):
     Enabled: bool
     PicoHwUid: str
+    FlowNodeName: SpaceheatName
     FlowMeterType: MakeModel = MakeModel.SAIER__SENHZG1WA
     HzCalcMethod: HzCalcMethod
     GpmFromHzMethod: GpmFromHzMethod

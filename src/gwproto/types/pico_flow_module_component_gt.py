fe173be26be6de29d065a06f7021b11086e598c9
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-10-26 23:45:01 -0400

    pico flow module HwUid is pico hwuid

--- src/gwproto/types/pico_flow_module_component_gt.py
+++ src/gwproto/types/pico_flow_module_component_gt.py
@@ -1,4 +1,7 @@
+import re
 from typing import Literal, Optional
+
+from pydantic import field_validator
 
 from gwproto.enums import GpmFromHzMethod, HzCalcMethod, MakeModel
 from gwproto.property_format import SpaceheatName
@@ -7,8 +10,8 @@ from gwproto.types.component_gt import ComponentGt
 
 class PicoFlowModuleComponentGt(ComponentGt):
     Enabled: bool
-    HwUid: str
-    PicoHwUid: str
+    SerialNumber: str
+    HwUid: str  # not optional - this is the pico HwUid
     FlowNodeName: SpaceheatName
     FlowMeterType: MakeModel = MakeModel.SAIER__SENHZG1WA
     HzCalcMethod: HzCalcMethod
@@ -27,3 +30,15 @@ class PicoFlowModuleComponentGt(ComponentGt):
     CutoffFrequency: Optional[float] = 1.25
     TypeName: Literal["pico.flow.module.component.gt"] = "pico.flow.module.component.gt"
     Version: Literal["000"] = "000"
+
+    @field_validator("HwUid")
+    @classmethod
+    def check_config_list(cls, v: str) -> str:
+        """
+        Axiom 1: HwUid is of the form 'pico_xxxxxx' where xxxxxx
+        are lowercase hex (the last digits of its pico W hw id)
+        """
+        pattern = r"^pico_[0-9a-f]{6}$"
+        if not bool(re.match(pattern, v)):
+            raise ValueError("HwUid should be the pico hwuid, eg pico_60e352")
+        return v

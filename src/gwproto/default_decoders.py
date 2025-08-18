5caf1253f1c883859274238cc5c04b1bb8229148
Jessica Millar <jessica.lynn.millar@gmail.com>
2025-08-18 15:21:00 -0500

    lazy import

--- src/gwproto/default_decoders.py
+++ src/gwproto/default_decoders.py
@@ -1,8 +1,6 @@
 # ruff: noqa: ANN401, RUF100,
 
 
-import gwproto.named_types.cacs
-import gwproto.named_types.components
 from gwproto.decoders import CacDecoder, ComponentDecoder
 
 __all__ = [
@@ -10,12 +8,24 @@ __all__ = [
     "default_component_decoder",
 ]
 
-default_cac_decoder = CacDecoder(
-    model_name="DefaultCacDecoder",
-    modules=[gwproto.named_types.cacs],
-)
 
-default_component_decoder = ComponentDecoder(
-    model_name="DefaultComponentDecoder",
-    modules=[gwproto.named_types.components],
-)
+def _get_default_cac_decoder() -> CacDecoder:
+    import gwproto.named_types.cacs
+
+    return CacDecoder(
+        model_name="DefaultCacDecoder",
+        modules=[gwproto.named_types.cacs],
+    )
+
+
+def _get_default_component_decoder() -> ComponentDecoder:
+    import gwproto.named_types.components
+
+    return ComponentDecoder(
+        model_name="DefaultComponentDecoder",
+        modules=[gwproto.named_types.components],
+    )
+
+
+default_cac_decoder = _get_default_cac_decoder()
+default_component_decoder = _get_default_component_decoder()

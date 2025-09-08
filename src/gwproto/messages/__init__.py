a9886b00327bb8f31d8fd0b8b3b9c07fa165355b
Jessica Millar <jessica.lynn.millar@gmail.com>
2025-09-08 17:55:59 -0400

    Add Header and Message to messages init

--- src/gwproto/messages/__init__.py
+++ src/gwproto/messages/__init__.py
@@ -1,5 +1,6 @@
 # ruff: noqa: F405, F403
 
+from gwproto.message import Header, Message
 from gwproto.named_types import *
 
 from .event import *
@@ -14,11 +15,13 @@ __all__ = [
     "EventBase",
     "EventMessage",
     "EventT",
+    "Header",
     "MQTTConnectEvent",
     "MQTTConnectFailedEvent",
     "MQTTDisconnectEvent",
     "MQTTFullySubscribedEvent",
     "MachineStates",
+    "Message",
     "PeerActiveEvent",
     "Ping",
     "PingMessage",

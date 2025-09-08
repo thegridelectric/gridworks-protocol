60f07ce8ac58f1898b473d5c857b18f2e93db384
Jessica Millar <jessica.lynn.millar@gmail.com>
2025-09-08 18:18:27 -0400

    Add MessageCategory and MessageCategorySymbol

---
+++ src/gwproto/enums/message_category.py
@@ -0,0 +1,46 @@
+from enum import auto
+
+from gw.enums import GwStrEnum
+
+
+class MessageCategory(GwStrEnum):
+    """
+    Categorizes message delivery patterns for ASL types. See [Message Delivery Architecture](https://gridworks.readthedocs.io/message-delivery-architecture/)
+    for details.
+    Values:
+      - Unknown: Default value when message pattern cannot be determined
+      - JsonDirect: Point-to-point delivery between specific actors Use for: commands,
+        responses, targeted telemetry
+      - JsonBroadcast: One-to-many delivery to multiple recipients Use for: market updates,
+        price and weather forecasts, shared data
+      - ScadaWrapped: Messages sent from SCADA systems that may require acknowledgement
+        tracking
+      - Serial: Bandwidth-optimized binary format (future) Use for: resource-constrained
+        scenarios, high-frequency data
+
+    For more information:
+        - [ASL Definition](https://raw.githubusercontent.com/thegridelectric/gridworks-asl/refs/heads/dev/type_definitions/enums/message.category.000.yaml)
+        - [GridWorks ASL Docs](https://gridworks-asl.readthedocs.io)
+    """
+
+    Unknown = auto()
+    JsonDirect = auto()
+    JsonBroadcast = auto()
+    ScadaWrapped = auto()
+    Serial = auto()
+
+    @classmethod
+    def default(cls) -> "MessageCategory":
+        return cls.Unknown
+
+    @classmethod
+    def values(cls) -> list[str]:
+        return [elt.value for elt in cls]
+
+    @classmethod
+    def enum_name(cls) -> str:
+        return "message.category"
+
+    @classmethod
+    def enum_version(cls) -> str:
+        return "000"
---
+++ src/gwproto/enums/message_category_symbol.py
@@ -0,0 +1,43 @@
+from enum import auto
+
+from gw.enums import GwStrEnum
+
+
+class MessageCategorySymbol(GwStrEnum):
+    """
+    Shorthand symbols used as the first element in GridWorks routing keys. See [Message Delivery
+    Architecture](https://gridworks.readthedocs.io/message-delivery-architecture/) for details.
+    Values:
+      - unknown: Default value when routing key parsing fails or symbol is not recognized
+      - rj: JsonDirect - point-to-point messages. Example: rj.hw1-keene-beech-scada.scada.power-watts.ltn.hw1-keene-beech
+      - rjb: JsonBroadcast - One-to-many messages with optional radio channel targeting
+        Examples: rjb.hw1-keene.marketmaker.market-list rjb.hw1-keene.marketmaker.latest-price.rt60gate5
+      - gw: ScadaWrapped - SCADA message with sckmowledgement tracking Example: gw.hw1-keene-beech-scada.to.a.report-event
+      - s: Serial - Future bandwidth-optimized format
+
+    For more information:
+        - [ASL Definition](https://raw.githubusercontent.com/thegridelectric/gridworks-asl/refs/heads/dev/type_definitions/enums/message.category.symbol.000.yaml)
+        - [GridWorks ASL Docs](https://gridworks-asl.readthedocs.io)
+    """
+
+    unknown = auto()
+    rj = auto()
+    rjb = auto()
+    gw = auto()
+    s = auto()
+
+    @classmethod
+    def default(cls) -> "MessageCategorySymbol":
+        return cls.unknown
+
+    @classmethod
+    def values(cls) -> list[str]:
+        return [elt.value for elt in cls]
+
+    @classmethod
+    def enum_name(cls) -> str:
+        return "message.category.symbol"
+
+    @classmethod
+    def enum_version(cls) -> str:
+        return "000"

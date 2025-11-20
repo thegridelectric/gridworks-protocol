1c124b078f1c1ad13bff52dd4b37390923e6d8e3
thdfw <th.defauw@gmail.com>
2025-11-20 17:06:49 +0100

    Adding sensor order to PicoTankModuleComponentGt

--- src/gwproto/named_types/pico_tank_module_component_gt.py
+++ src/gwproto/named_types/pico_tank_module_component_gt.py
@@ -22,7 +22,7 @@ class PicoTankModuleComponentGt(ComponentGt):
     PicoKOhms: Optional[int] = None
     SerialNumber: str = "NA"
     AsyncCaptureDeltaMicroVolts: int
-    SwapTopBottom: bool = False
+    SensorOrder: list[int] = [1,2,3]
     TypeName: Literal["pico.tank.module.component.gt"] = "pico.tank.module.component.gt"
     Version: str = "010"
 
